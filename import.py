060f00a5b073a1bd54307880c85f002ae27d6150
pkdyn <puneetsk21@gmail.com>
2022-03-28 10:56:10 +0530

    podcast-semanticsearch

---
+++ import.py
@@ -0,0 +1,56 @@
+import weaviate
+import json
+import helper
+
+client = weaviate.Client("http://localhost:8881")
+client.timeout_config = (6000)
+
+client.schema.delete_all()
+schema = {
+    "classes": [
+        {
+            "class": "Podcast",
+            "properties": [
+                {
+                    "name": "title",
+                    "dataType": ["text"]
+                },
+                {
+                    "name": "transcript",
+                    "dataType": ["text"]
+                }
+            ]
+        }
+    ]
+}
+client.schema.create(schema)
+
+with open("data/podcast.json", 'r') as f:
+    datastore = json.load(f)
+    
+def add_podcasts(batch_size=1, debug_mode=False):
+    no_items_in_batch = 0
+    for item in datastore:
+        podcast_object = {
+            "title": item["title"],
+            "transcript": item["transcript"]
+        }
+
+        podcast_uuid = helper.generate_uuid('podcast', item["title"] + item["transcript"])
+        client.batch.add_data_object(podcast_object, "Podcast", podcast_uuid)
+        no_items_in_batch += 1
+
+        if no_items_in_batch >= batch_size:
+            results = client.batch.create_objects()
+
+            if debug_mode:
+                for result in results:
+                    if result['result'] != {}:
+                        helper.log(result['result'])
+
+            no_items_in_batch = 0
+
+    client.batch.create_objects()
+    
+add_podcasts()
+
