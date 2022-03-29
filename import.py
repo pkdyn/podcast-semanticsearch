dca10a530bef6c7c2023d5ef8aa8fed820fcf12d
pkdyn <puneetsk21@gmail.com>
2022-03-30 01:01:35 +0530

    changes for 300 ep ds

--- import.py
+++ import.py
@@ -2,8 +2,8 @@ import weaviate
 import json
 import helper
 
-client = weaviate.Client("http://localhost:8881")
-client.timeout_config = (12000) #Increase/Decrease value for larger/smaller dataset size. 
+client = weaviate.Client("http://localhost:9999")
+client.timeout_config = (10000) 
 
 client.schema.delete_all()
 schema = {
@@ -28,7 +28,7 @@ client.schema.create(schema)
 with open("data/podcast_ds.json", 'r') as f:
     datastore = json.load(f)
     
-def add_podcasts(batch_size=1, debug_mode=False):
+def add_podcasts(batch_size = 1):
     no_items_in_batch = 0
     for item in datastore:
         podcast_object = {
@@ -36,24 +36,23 @@ def add_podcasts(batch_size=1, debug_mode=False):
             "transcript": item["transcript"]
         }
 
-        podcast_uuid = helper.generate_uuid('podcast', item["title"] + item["transcript"])
+        podcast_uuid = helper.generate_uuid('podcast', item["title"] + item["url"])
         client.batch.add_data_object(podcast_object, "Podcast", podcast_uuid)
         no_items_in_batch += 1
 
         if no_items_in_batch >= batch_size:
             results = client.batch.create_objects()
 
-            if debug_mode:
-                for result in results:
+            for result in results:
                     if result['result'] != {}:
                         helper.log(result['result'])
 
-                message = str(item["title"]) + ' imported'
-                helper.log(message)
+            message = str(item["title"]) + ' imported'
+            helper.log(message)
 
             no_items_in_batch = 0
 
     client.batch.create_objects()
     
-add_podcasts(1, True)
+add_podcasts(1)
 
